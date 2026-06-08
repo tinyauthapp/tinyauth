b90f95a17df35b1d7d8e2f61b3bd3925887eb27f
Ryc O'Chet <rycochet@rycochet.com>
2026-06-08 12:55:01 +0100

    Fix missing import

--- internal/service/ldap_service.go
+++ internal/service/ldap_service.go
@@ -11,6 +11,7 @@ import (
 	ldapgo "github.com/go-ldap/ldap/v3"
 	"github.com/steveiliop56/ding"
 	"github.com/tinyauthapp/tinyauth/internal/model"
+	"github.com/tinyauthapp/tinyauth/internal/utils"
 	"github.com/tinyauthapp/tinyauth/internal/utils/logger"
 )
 
